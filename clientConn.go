b88cb87145fce9073fec9d73ee8c1da11a5569f1
Darren Shepherd <darren@rancher.com>
2015-12-29 21:15:04 -0700

    Add a timeout to the socket read
    Signed-off-by: Darren Shepherd <darren@rancher.com>

--- clientConn.go
+++ clientConn.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"time"
+)
+
+const (
+	timeout = 15
 )
 
 // This object is not thread safe.
@@ -50,8 +55,8 @@ func (c *ClientConn) Request(apiname string, request map[string]interface{}) (re
 		fmt.Printf("error writing segment \n")
 		return
 	}
-	outMsg := <-c.responseChan
 
+	outMsg := c.readResponse()
 	if c.lastError != nil {
 		return nil, c.lastError
 	}
@@ -59,6 +64,18 @@ func (c *ClientConn) Request(apiname string, request map[string]interface{}) (re
 		return nil, fmt.Errorf("[%s] response error %d", apiname, outMsg.typ)
 	}
 	return outMsg.msg, nil
+}
+
+func (c *ClientConn) readResponse() segment {
+	select {
+	case outMsg := <-c.responseChan:
+		return outMsg
+	case <-time.After(timeout * time.Second):
+		if c.lastError == nil {
+			c.lastError = fmt.Errorf("Timeout waiting for message response")
+		}
+		return segment{}
+	}
 }
 
 func (c *ClientConn) RegisterEvent(name string, handler func(response map[string]interface{})) (err error) {
@@ -74,7 +91,7 @@ func (c *ClientConn) RegisterEvent(name string, handler func(response map[string
 		delete(c.eventHandlers, name)
 		return
 	}
-	outMsg := <-c.responseChan
+	outMsg := c.readResponse()
 	//fmt.Printf("registerEvent %#v\n", outMsg)
 	if c.lastError != nil {
 		delete(c.eventHandlers, name)
@@ -96,7 +113,7 @@ func (c *ClientConn) UnregisterEvent(name string) (err error) {
 	if err != nil {
 		return
 	}
-	outMsg := <-c.responseChan
+	outMsg := c.readResponse()
 	//fmt.Printf("UnregisterEvent %#v\n", outMsg)
 	if c.lastError != nil {
 		return c.lastError
