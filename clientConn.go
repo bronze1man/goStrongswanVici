df5a9cb047cd1c41bf29e33a7653bf2ff99989dc
YafimK <yafim.kazak@gmail.com>
2020-08-08 12:01:50 +0300

    fmt fix lock conditions

--- clientConn.go
+++ clientConn.go
@@ -32,6 +32,7 @@ func (c *ClientConn) Close() error {
 	defer c.lock.Unlock()
 	close(c.responseChan)
 	c.lastError = io.ErrClosedPipe
+
 	return c.conn.Close()
 }
 
@@ -43,6 +44,7 @@ func NewClientConn(conn net.Conn) (client *ClientConn) {
 		ReadTimeout:   DefaultReadTimeout,
 	}
 	go client.readThread()
+
 	return client
 }
 
@@ -52,6 +54,7 @@ func NewClientConnFromDefaultSocket() (client *ClientConn, err error) {
 	if err != nil {
 		return
 	}
+
 	return NewClientConn(conn), nil
 }
 
@@ -63,18 +66,23 @@ func (c *ClientConn) Request(apiname string, request map[string]interface{}) (re
 	})
 	if err != nil {
 		fmt.Printf("error writing segment \n")
+
 		return
 	}
+
 	outMsg := c.readResponse()
 	c.lock.RLock()
 	err = c.lastError
-	c.lock.RUnlock()
 	if err != nil {
+		c.lock.RUnlock()
 		return nil, err
 	}
+	c.lock.RUnlock()
+
 	if outMsg.typ != stCMD_RESPONSE {
 		return nil, fmt.Errorf("[%s] response error %d", apiname, outMsg.typ)
 	}
+
 	return outMsg.msg, nil
 }
 
@@ -88,16 +96,18 @@ func (c *ClientConn) readResponse() segment {
 			c.lastError = fmt.Errorf("Timeout waiting for message response")
 			c.lock.Unlock()
 		}
+
 		return segment{}
 	}
 }
 
 func (c *ClientConn) RegisterEvent(name string, handler func(response map[string]interface{})) (err error) {
 	c.lock.Lock()
-	defer c.lock.Unlock()
 	if c.eventHandlers[name] != nil {
+		c.lock.Unlock()
 		return fmt.Errorf("[event %s] register a event twice.", name)
 	}
+
 	c.eventHandlers[name] = handler
 	err = writeSegment(c.conn, segment{
 		typ:  stEVENT_REGISTER,
@@ -105,25 +115,35 @@ func (c *ClientConn) RegisterEvent(name string, handler func(response map[string
 	})
 	if err != nil {
 		delete(c.eventHandlers, name)
+		c.lock.Unlock()
+
 		return
 	}
+	c.lock.Unlock()
 	outMsg := c.readResponse()
-	//fmt.Printf("registerEvent %#v\n", outMsg)
-	if c.lastError != nil {
+	// fmt.Printf("registerEvent %#v\n", outMsg)
+	c.lock.Lock()
+	lastError := c.lastError
+
+	if lastError != nil {
 		delete(c.eventHandlers, name)
-		return c.lastError
+		c.lock.Unlock()
+
+		return err
 	}
 
 	if outMsg.typ != stEVENT_CONFIRM {
 		delete(c.eventHandlers, name)
+		c.lock.Unlock()
+
 		return fmt.Errorf("[event %s] response error %d", name, outMsg.typ)
 	}
+	c.lock.Unlock()
+
 	return nil
 }
 
 func (c *ClientConn) UnregisterEvent(name string) (err error) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
 	err = writeSegment(c.conn, segment{
 		typ:  stEVENT_UNREGISTER,
 		name: name,
@@ -131,16 +151,25 @@ func (c *ClientConn) UnregisterEvent(name string) (err error) {
 	if err != nil {
 		return
 	}
+
 	outMsg := c.readResponse()
-	//fmt.Printf("UnregisterEvent %#v\n", outMsg)
+	// fmt.Printf("UnregisterEvent %#v\n", outMsg)
+	c.lock.Lock()
 	if c.lastError != nil {
+		c.lock.Unlock()
+
 		return c.lastError
 	}
+	c.lock.Unlock()
 
 	if outMsg.typ != stEVENT_CONFIRM {
 		return fmt.Errorf("[event %s] response error %d", name, outMsg.typ)
 	}
+
+	c.lock.Lock()
 	delete(c.eventHandlers, name)
+	c.lock.Unlock()
+
 	return nil
 }
 
@@ -151,8 +180,10 @@ func (c *ClientConn) readThread() {
 			c.lock.Lock()
 			c.lastError = err
 			c.lock.Unlock()
+
 			return
 		}
+
 		switch outMsg.typ {
 		case stCMD_RESPONSE, stEVENT_CONFIRM:
 			c.responseChan <- outMsg
@@ -160,6 +191,7 @@ func (c *ClientConn) readThread() {
 			c.lock.Lock()
 			handler := c.eventHandlers[outMsg.name]
 			c.lock.Unlock()
+
 			if handler != nil {
 				handler(outMsg.msg)
 			}
@@ -167,6 +199,7 @@ func (c *ClientConn) readThread() {
 			c.lock.Lock()
 			c.lastError = fmt.Errorf("[Client.readThread] unknow msg type %d", outMsg.typ)
 			c.lock.Unlock()
+
 			return
 		}
 	}
